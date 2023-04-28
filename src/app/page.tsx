aa8dca8b9966b26b28a55ba3892c2f098e1984b6
Maximilian Zauner <zaunermax@gmail.com>
2023-04-28 18:55:01 +0200

    feat(analytics): added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { UserInput } from '@/components/user-input';
 import { InfoSection } from '@/components/info-section';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
 	return (
-		<div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+		<div className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
 			<UserInput />
 			<InfoSection />
+			<Analytics />
 		</div>
 	);
 }
